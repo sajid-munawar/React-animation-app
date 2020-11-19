0a73127081ef1dbf345a6b3358ed11872e7c718a
sajid-munawar <sajidmunawar666@gmail.com>
2020-11-19 11:38:29 +0500

    implemented simply ribrary

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import useWebAnimations from "@wellyshen/use-web-animations";
 
-function App() {
+const App = () => {
+  const { ref, playState } = useWebAnimations({
+    keyframes: {
+      transform: "translateX(500px)", // Move by 500px
+      background: ["red", "blue", "green"], // Go through three colors
+    },
+    timing: {
+      delay: 500, // Start with a 500ms delay
+      duration: 1000, // Run for 1000ms
+      iterations: 2, // Repeat once
+      direction: "alternate", // Run the animation forwards and then backwards
+      easing: "ease-in-out", // Use a fancy timing function
+    },
+  //   onReady: ({ playState, animate, animation }) => {
+  //     // Triggered when the animation is ready to play (Google Chrome: available in v84+)
+  //   },
+  //   onUpdate: ({ playState, animate, animation }) => {
+  //     // Triggered when the animation enters the running state or changes state
+  //   },
+  //   onFinish: ({ playState, animate, animation }) => {
+  //     // Triggered when the animation enters the finished state (Google Chrome: available in v84+)
+  //   },
+  //   // More useful options...
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <p>🍿 Animation is {playState}</p>
+      <div className="target" ref={ref} />
+      Hello
     </div>
   );
-}
-
+};
 export default App;
